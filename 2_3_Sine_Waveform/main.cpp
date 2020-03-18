92534467ede4e04a25e482732b63cdea60585980
johnny <johnnyjohnny6201@gmail.com>
2020-03-18 18:08:17 +0800

    2_4

---
+++ 2_3_Sine_Waveform/main.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+
+AnalogOut Aout(DAC0_OUT);
+
+int main(void)
+{
+    float i;
+    
+    while(1) {
+        for(i = 0;i < 2;i += 0.05) {
+            Aout = 0.5 + 0.5*sin(i*3.14159);
+            wait(0.001);
+        }
+    }
+
+    return 0;
+}
